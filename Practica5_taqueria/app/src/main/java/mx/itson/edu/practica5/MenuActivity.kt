0d6407416faadea9ce57b8fa444acf7a13d9be44
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2024-02-21 12:55:17 -0700

    Práctica terminada
    Pero no compila por la versión de kotlin

--- Practica5_taqueria/app/src/main/java/mx/itson/edu/practica5/MenuActivity.kt
+++ Practica5_taqueria/app/src/main/java/mx/itson/edu/practica5/MenuActivity.kt
@@ -1,11 +1,28 @@
 package mx.itson.edu.practica5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnAntojitos:Button=findViewById(R.id.btn_antojitos)
+        var btnEspecialidades:Button=findViewById(R.id.btn_especialidades)
+
+        btnAntojitos.setOnClickListener {
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Antojitos")
+            startActivity(intent)
+        }
+
+        btnEspecialidades.setOnClickListener {
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Especialidades")
+            startActivity(intent)
+        }
     }
 }
