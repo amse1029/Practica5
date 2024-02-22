f1f2d74d50aebd852eb8594c797b88a1d5fa2739
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2024-02-22 16:02:35 -0700

    Práctica terminada y revisada

--- Practica5_taqueria/app/src/main/java/mx/itson/edu/practica5/MenuActivity.kt
+++ Practica5_taqueria/app/src/main/java/mx/itson/edu/practica5/MenuActivity.kt
@@ -12,6 +12,10 @@ class MenuActivity : AppCompatActivity() {
 
         var btnAntojitos:Button=findViewById(R.id.btn_antojitos)
         var btnEspecialidades:Button=findViewById(R.id.btn_especialidades)
+        var btnCombinations:Button=findViewById(R.id.btn_combinations)
+        var btnTortas:Button=findViewById(R.id.btn_tortas)
+        var btnSopas:Button=findViewById(R.id.btn_sopas)
+        var btnDrinks:Button=findViewById(R.id.btn_drinks)
 
         btnAntojitos.setOnClickListener {
             var intent: Intent =Intent(this, ProductosActivity::class.java)
@@ -22,6 +26,30 @@ class MenuActivity : AppCompatActivity() {
         btnEspecialidades.setOnClickListener {
             var intent: Intent =Intent(this, ProductosActivity::class.java)
             intent.putExtra("menuType", "Especialidades")
+            startActivity(intent)
+        }
+
+        btnCombinations.setOnClickListener {
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Combinations")
+            startActivity(intent)
+        }
+
+        btnTortas.setOnClickListener {
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Tortas")
+            startActivity(intent)
+        }
+
+        btnSopas.setOnClickListener {
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Sopas")
+            startActivity(intent)
+        }
+
+        btnDrinks.setOnClickListener {
+            var intent: Intent =Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Drinks")
             startActivity(intent)
         }
     }
